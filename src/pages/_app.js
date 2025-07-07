6f7a35e6a546881d813ac17a7cf357e4fbd8c9a4
Matteus Moreno <matteus_moreno@live.com>
2025-07-07 20:48:03 -0300

    update _app.js for google search

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,13 +4,19 @@ import { NotificationProvider } from '@/contexts/NotificationContext';
 import GlobalStyles from '@/styles/GlobalStyles';
 import { theme } from '@/styles/theme';
 import Head from 'next/head';
+import { useRouter } from 'next/router'; // 1. IMPORTAR O useRouter
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter(); // 2. INICIAR O HOOK
+  const canonicalUrl = `https://www.casa-moreno.com${router.asPath === '/' ? '' : router.asPath}`.split('?')[0]; // 3. CONSTRUIR A URL CANÔNICA
+
   return (
     <>
       <Head>
         <title>Casa Moreno - A melhor loja do mundo</title>
         <meta name="description" content="Encontre as melhores ofertas em smartphones e eletrônicos na Casa Moreno." />
+        {/* 4. ADICIONAR A TAG CANÔNICA */}
+        <link rel="canonical" href={canonicalUrl} />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
