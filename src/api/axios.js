e4070c2b76e8c0a96ce7310d6c41457bd32e3b54
Matteus Moreno <matteus_moreno@live.com>
2025-07-24 11:29:37 -0300

    update backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://www.casa-moreno.com',
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   //baseURL: 'http://localhost:8085',
 });
 
